f0ddab188723eaa69305704afc96b6dd77ebb145
Martin Lujan <martinlujan96@gmail.com>
2018-10-19 20:05:27 -0300

    add funciones y doc

--- TP2/main.c
+++ TP2/main.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * Utilizada para la obtencion del path del sitema.
+ * @return
+ */
+int getPath();
+/**
+ * Utilizada para la obtencion de hostname y user
+ * @param recibe dos puntero char para almacenar el hostname y user.
+ * @return ?
+ */
+void getHostUser(char*, char*);
 
 /**
- * RECORDAR: unisegned sin signo
+ * Proyecto Baash, se implementara una terminal del sistema, en la cual
+ * se le ingresaran comandos asigados al path y que devolvera por pantalla lo que
+ * se solicite
  * @TODO TODO LO Q HAY Q HACER PAPUUUUU
+ * @param argc tipo entero el cual lleva la cuenta de los parametros que se ingresan.
+ * @param argv puntero tipo char, de tamaño dinamico, en el que se guardaran los parametros ingresados.
  * @return 0
  */
-
 int main (int argc, char **argv)
 {
+
+  printf ("esto no hace una goma");
   return 0;
+}
+
+void getHostUser(user, host)
+{
+ int i;
 }
