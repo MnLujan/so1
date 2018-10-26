368d6d364427de8176aaa5dc69aae5aad3447a13
Martin Lujan <martinlujan96@gmail.com>
2018-10-26 15:46:59 -0300

    add fuction main

--- TP2/baash.c
+++ TP2/baash.c
@@ -1,35 +1,47 @@
+/**
+ * @author Martin Lujan
+ * Sistemas Operativos 2018
+ *
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
 
+#define BUFFERSIZE 256;
 /**
  * @brief Utilizada para la obtencion del path del sitema.
  * @return
  */
 int getPath();
-/**
- * @brief Utilizada para la obtencion de hostname y user
- * @param recibe dos puntero char para almacenar el hostname y user.
- * @return ?
- */
-void getHostUser(char*, char*);
 
 /**
  * @brief Baash, se implementara una terminal del sistema, en la cual
  * se le ingresaran comandos asigados al path y que devolvera por pantalla lo que
  * se solicite
- * @TODO TODO LO Q HAY Q HACER PAPUUUUU
+ * @TODO Implementar la entrada por teclado de parametros.
  * @param argc tipo entero el cual lleva la cuenta de los parametros que se ingresan.
  * @param argv puntero tipo char, de tamaño dinamico, en el que se guardaran los parametros ingresados.
  * @return 0
  */
-int main (int argc, char **argv)
+int main ()
 {
+  char hostname[20];
+  char* user;
+  gethostname (hostname,20);
+  user = getlogin ();
+  char input[256];
+
+
+
+  while(1){
+
+      printf ("%s@%s %s$ ", user, hostname, getcwd(NULL,256));
+      fgets (input, 256, stdin);
+      printf ("se ingreso %s", input);
+      break;
+  }
+
   return 0;
 }
 
-void getHostUser(char* host, char* user)
-{
-  
-
-}
