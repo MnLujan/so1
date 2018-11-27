a60fafa000fe8a3e30789dc998964121386daa48
Martin Lujan <martinlujan96@gmail.com>
2018-11-27 19:47:04 -0300

    Add getCommands

--- TP2/baash.c
+++ TP2/baash.c
@@ -14,81 +14,69 @@
  * @brief Utilizada para la obtencion del path del sitema.
  * @return
  */
-int getPath ();
+int getPath();
+
 /**
  * @brief Utilizada para la extraccion de comandos y argumentos de la linea ingresada por el teclado
  * @param Se le envia un puntero a un vector el cual almacena los comandos ingresados.
  * @param Se envia un tipo entero para contabilizar la cantidad de argumentos ingresados.
  * @return 0.
  */
-char getCommands(char);
+void getCommands(char* argv1[], char* input1);
 
 /**
  * @brief Baash, se implementara una terminal del sistema, en la cual
  * se le ingresaran comandos asigados al path y que devolvera por pantalla lo que
  * se solicite
- * @TODO Implementar la entrada por teclado de parametros.
+ * @TODO Obetencion de PATH, busqueda relativa y absoluta.
  * @return 0
  */
-int main ()
-{
-  char hostname[20];
-  char *user;
-  gethostname (hostname, 20);
-  user = getlogin ();
-  char input[256];
-  char argv;
-
-  while (1)
-    {
-
-      printf ("%s@%s %s$ ", user, hostname, getcwd (NULL, 256));
-      /* Esperaremos el ingreso por teclado */
-      fgets (input, 256, stdin);
-
-      if (!strcmp (input, "\n"))
-        {
-          printf ("\n");
-          continue;
-        }
-      else if (strcmp (input, "exit"))
-        {
-          break;
-        }
-      else
-        {
-          argv = getCommands (input);
-
-          continue;
-        }
-
-    }
-
-  return 0;
-}
 
 #define buffer 256
+int main() {
+    char hostname[20];
+    char *user;
+    gethostname(hostname, 20);
+    user = getlogin();
+    char input[buffer];
+    char* argv[20];
 
-char getCommands(char line){
+    while (1) {
 
-  char **tokens = malloc(256);
-  char *token;
-  int pos = 0;
+        printf("%s@%s %s$ ", user, hostname, getcwd(NULL, 256));
+        /* Esperaremos el ingreso por teclado */
+        fgets(input, buffer, stdin);
 
-  if(!tokens){
-    printf ("Allocation error \n");
-    exit (EXIT_FAILURE);
-  }
+        if (!strcmp(input,"\n")) {
+            printf("\n");
+            continue;
 
-  token = strtok (line, buffer);
+        } else {
+            getCommands(argv,input);
 
-  while(token != NULL){
-    tokens[pos] = token;
-    pos++;
+            if (!strcmp(input,"exit\n")) {
+                return 0;
+            } else if (!strcmp(argv, "cd")){
+                //chdir(argv[1]);
+                continue;
+            }
+        }
+    }
 
-    token = strtok (NULL, buffer);
-  }
-  tokens[pos] = NULL;
-  return tokens;
+}
+
+void getCommands(char* argv1[], char* input1) {
+int argc = 0;
+argv1[argc] = strtok(input1," ");
+//argc++;
+
+while (argv1[argc] != NULL){
+    printf("%s\n", argv1[argc]);
+    argc ++;
+    argv1[argc] = strtok (NULL," \n");
+
+}
+
+return;
 
 }
