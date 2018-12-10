73dbbb89b5993e247621013a5888eaf8ece3b913
Martin Lujan <martinlujan96@gmail.com>
2018-12-09 23:43:51 -0300

    fix en general

--- TP3/main.c
+++ TP3/main.c
@@ -2,15 +2,17 @@
 #include <zconf.h>
 #include <string.h>
 
-# define align4 (x)(((((x)-1)>>2)<<2)+4)
-# define BLOCK_SIZE 20
+# define BLOCK_SIZE 48
+# define align4(x) (((((x)-1)>>2)<<2)+4)
 
+
+/* Bloque de meta data */
 struct s_block {
-    size_t size;
-    struct s_block *next;
-    struct s_block *prev;
-    int free;
-    void *ptr;
+    size_t size; //Tamaño del bloque
+    struct s_block *next; //Proximo nodo
+    struct s_block *prev; //Nodo anterior
+    int free; //Si se encuentra libre o no
+    void *ptr; //puntero a bloque alocado
     /* Puntero al bloque asignado */
     char data[1];
 };
@@ -37,38 +39,52 @@ void *mallocFalse(size_t size);
 
 int main() {
     /* Punteros a char e int para probar las funciones */
-    char *string;
-    int *array;
-    void *test;
+    int i = 1;
+    char *string; //4bytes un int
+    //char array[1];
+    // int *array;
+    // void *test;
 
-    string = (char *) mallocFalse(20);
-    test = sbrk(0);
-    if (string != NULL) {
-        printf("Test realizado con exito\n");
-        array = (int *) mallocFalse(20);
-        if (array != NULL) {
-            printf("2do Test realizado con exito\n");
-        } else {
-            printf("ERROR al alocar memoria 1\n");
-        }
-    } else {
-        printf("ERROR al alocar memoria");
+    string = (char *) mallocFalse(1);
+   //array = string;
+    for (i; i <= 10; i++) {
+        *string = 'a';
+        string++;
     }
-
+    //printf("%i \n", (int)(sizeof('a')));
+    //printf("%i \n", (int)(sizeof(struct s_block)));
+    //printf("%i \n", (int)(sizeof(char [1])));
+    //printf("holis");
+    i = 1;
     free(string);
-    free(array);
+    /*
+    for (i; i <= 10; i++) {
+        *string = 'a';
+        string++;
+    }*/
+    // free(array);
     return 0;
 }
 
+/**
+ * Muevo el break pointer en "s" lugares, partiendo desde la posicion Last.
+ * @param last puntero al ultimo break.
+ * @param s tamaño que se quiere expandir.
+ * @return un puntero a la estructura s_block
+ */
 t_block extend_heap(t_block last, size_t s) {
     t_block b;
     b = sbrk(0);
-    if (sbrk(BLOCK_SIZE + s) == (void *) -1) {
-/* sbrk falla, devuelve NULL */
+
+    if (sbrk(sizeof(struct s_block) + s) == (void *) -1) {
+   /* sbrk falla, devuelve NULL */
         return (NULL);
     }
+
     b->size = s;
     b->next = NULL;
+    b->prev = last;
+    b->ptr = b->data;
     if (last) {
         last->next = b;
     } else {
@@ -78,29 +94,59 @@ t_block extend_heap(t_block last, size_t s) {
     return (b);
 }
 
+/**
+ * Busca el bloque de memoeria necesario para otro bloque.
+ * @param last Ultimo bloque visitado
+ * @param size tamaño a expandir
+ * @return puntero a la estructura s_block
+ */
 t_block find_block(t_block *last, size_t size) {
     t_block b = base;
+
+    /* Mientras exista un bloque y no este libre, ni tenga suficiente memoria */
     while (b && !(b->free && b->size >= size)) {
+
+        /* Indico el ultimo bloque visitado y me voy al siguiente */
         *last = b;
         b = b->next;
     }
     return (b);
 }
 
+/**
+ * Se encarga de crear un nuevo bloque desplazado en "s lugares de la posicion actual "b".
+ * @param b puntero a bloque.
+ * @param s tamaño requerido.
+ */
 void split_block(t_block b, size_t s) {
     t_block new;
+
+    /* Inicializo el nuevo bloque */
     new = (t_block) (b->data + s);
     new->size = b->size - s - BLOCK_SIZE;
     new->next = b->next;
     new->free = 1;
+    new->ptr = new->data;
+
+    /* Modifico el bloqu actual para que tenga en cuenta el nuevo bloque */
     b->size = s;
     b->next = new;
+    if(new->next){
+        new->next->prev = new;
+    }
 }
 
+/**
+ * Se encarga de asignar memoria a nuestro programa a medida que se lo vallamos pidiendo.
+ * @param size tamaño que se quiere expandir.
+ * @return puntero a la posicion de memoria.
+ */
 void *mallocFalse(size_t size) {
     t_block b, last;
     size_t s;
-    // s = align4(size);
+
+    /* Alineado de punteros */
+     s = align4(size);
     if (base) {
 /* Primero encuentra el bloque */
         last = base;
@@ -120,13 +166,18 @@ void *mallocFalse(size_t size) {
 
 /* Primera vez */
         b = extend_heap(NULL, s);
-        if (!b)
+        if (!b) {
             return (NULL);
+        }
         base = b;
     }
-    return (b->data);
+    return (b->ptr);
 }
-
+/**
+ * Realiza la fusion del bloque actual con el siguiente.
+ * @param b puntero al bloque a fusionar.
+ * @return puntero al bloque fusionado.
+ */
 t_block fusion(t_block b) {
     if (b->next && b->next->free) {
         b->size += BLOCK_SIZE + b->next->size;
@@ -137,16 +188,21 @@ t_block fusion(t_block b) {
     return (b);
 }
 
+/**
+ * Nos devuelve un puntero a la estructura con toda la informacion del bloque
+ * @param p puntero a la direccion de memoria.
+ * @return mismo puntero pero a la informacion del bloque.
+ */
 t_block get_block(void *p) {
     char *tmp;
     tmp = p;
-    return (p = tmp -= BLOCK_SIZE);
+    return (p = tmp - BLOCK_SIZE);
 }
 
 /**
- * Verifica si la direccion libre es valida
- * @param p
- * @return
+ * Verifica si la direccion apunta a un bloque valido.
+ * @param p puntero al bloque.
+ * @return los datos del bloque, un cero si es invalido.
  */
 int valid_addr(void *p) {
     if (base) {
@@ -154,12 +210,16 @@ int valid_addr(void *p) {
             return (p == (get_block(p))->ptr);
         }
     }
-    return (0);
+    return 0;
 }
 
+/**
+ * Libera la memoria deseada.
+ * @param p puntero a la direccion de memoria a liberar,
+ */
 void free(void *p) {
     t_block b;
-    if (valid_addr(p)) {
+    if (!valid_addr(p)) {
         b = get_block(p);
         b->free = 1;
 /* Fusion con la anterior si es posible */
@@ -173,9 +233,9 @@ void free(void *p) {
             if (b->prev)
                 b->prev->next = NULL;
             else
-/* No mas bloques */
+/* Si no hay previo es porq libere todos los bloques*/
                 base = NULL;
-            brk(b);
+            brk(b); //Borrado del bloque actual.
         }
     }
 }
